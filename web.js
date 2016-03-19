825ce193bf4aba119fb943b6244e3084887816b9
supercaracal <supercaracal@yahoo.co.jp>
2016-03-19 11:56:07 +0900

    fix absolute terror field works

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var server = http.createServer(app);
 server.listen(process.env.PORT || 8080);
 app.get('/', function(req, res) {
     res.header('Cache-Control', 'public, max-age=1800');
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 app.get('/*', function(req, res) {
     res.header('Cache-Control', 'public, max-age=1800');
-    res.sendfile(__dirname + req.url);
+    res.sendFile(__dirname + req.url);
 });
