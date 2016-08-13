267ceb18dc01612c96ce83487ea4def4a0c437cb
supercaracal <supercaracal@yahoo.co.jp>
2016-08-13 14:19:52 +0900

    Fix touch event bugs in chrome

--- atfield.js
+++ atfield.js
@@ -1,60 +1,90 @@
 var ATField = Class.create({
   SVG_NS: null,
   XLINK_NS: null,
+  hasMousedownEvent: null,
   hasTouchEvent: null,
   hasAudioElm: null,
   svgElm: null,
+
   initialize: function(svgId) {
     this.SVG_NS = 'http://www.w3.org/2000/svg';
     this.XLINK_NS = 'http://www.w3.org/1999/xlink';
-    // MIXME:
-    // this.hasTouchEvent = typeof new Element('div', {ontouchstart: 'return;'}).ontouchstart == 'function';
-    this.hasTouchEvent = false;
-    var canPlayMpeg = typeof Audio == 'function' && Audio.name == 'HTMLAudioElement' &&
-        typeof Audio.prototype.canPlayType == 'function' && new Audio().canPlayType('audio/mpeg');
+    this.hasMousedownEvent = typeof new Element('div', {onmousedown: 'return;'}).onmousedown == 'function';
+    this.hasTouchEvent = typeof new Element('div', {ontouchstart: 'return;'}).ontouchstart == 'function';
+    var canPlayMpeg = typeof Audio == 'function' &&
+                      Audio.name == 'HTMLAudioElement' &&
+                      typeof Audio.prototype.canPlayType == 'function' &&
+                      new Audio().canPlayType('audio/mpeg');
     this.hasAudioElm = canPlayMpeg == 'probably' || canPlayMpeg == 'maybe';
     this.svgElm = $(svgId);
     this.setEventListener.bind(this).delay(0.5);
   },
+
   setEventListener: function() {
     var body = $(document.body);
-    body.observe(this.hasTouchEvent ? 'touchstart' : 'click',
-      this.deployATField.bindAsEventListener(this));
+    if (this.hasMousedownEvent) {
+      body.observe('mousedown', this.deployATField.bindAsEventListener(this));
+    }
+    if (this.hasTouchEvent) {
+      body.observe('touchstart', this.deployATField.bindAsEventListener(this));
+    }
   },
+
   deployATField: function(ev) {
+    var x, y;
     this.playSound();
-    var x = this.hasTouchEvent ? ev.touches[0].pageX : ev.pageX ,
-      y = this.hasTouchEvent ? ev.touches[0].pageY : ev.pageY;
+    if (ev.type === 'mousedown') {
+      x = ev.pageX;
+      y = ev.pageY;
+    } else {
+      x = ev.touches[0].pageX;
+      y = ev.touches[0].pageY;
+    }
     ev.stop();
     this.ripple(x, y, 50);
   },
+
   playSound: function() {
-    if (!this.hasAudioElm) return;
+    if (!this.hasAudioElm) {
+      return;
+    }
     var sound = new Element('audio', {src: '/at_field.mp3'});
-    if (Prototype.Browser.MobileSafari) sound.load();
+    if (Prototype.Browser.MobileSafari) {
+      sound.load();
+    }
     try {
       sound.play();
     } catch(e) {
-      if (console) console.log(e);
+      if (console) {
+        console.log(e);
+      }
     }
   },
+
   ripple: function(x, y, r) {
-    if (300 < r) return;
+    if (300 < r) {
+      return;
+    }
     var elm = this.createOctagon(this.generateOctagonPoints(x, y, r));
     this.insertOctagon(elm);
     this.ripple.bind(this).delay(0.03, x, y, r + 50);
-    if ('remove' in elm) elm.remove.bind(elm).delay(3);
+    if ('remove' in elm) {
+      elm.remove.bind(elm).delay(3);
+    }
   },
+
   createOctagon: function(points) {
     var elm = document.createElementNS(this.SVG_NS, 'polygon');
-    elm.setAttributeNS(null, 'points', points.map(function(x) { return x.join(','); }).join(' '));
-    elm.setAttributeNS(null, 'stroke', '#FF6600');
-    elm.setAttributeNS(null, 'fill', 'url(#grad01)');
-    elm.setAttributeNS(null, 'stroke-width', '1');
+    var point_str =  points.map(function(x) { return x.join(','); }).join(' ');
+    elm.setAttributeNS(null, 'points',         point_str);
+    elm.setAttributeNS(null, 'stroke',         '#FF6600');
+    elm.setAttributeNS(null, 'fill',           'url(#grad01)');
+    elm.setAttributeNS(null, 'stroke-width',   '1');
     elm.setAttributeNS(null, 'stroke-opacity', '0.9');
-    elm.setAttributeNS(null, 'opacity', '0.9');
+    elm.setAttributeNS(null, 'opacity',        '0.9');
     return elm;
   },
+
   generateOctagonPoints: function(cx, cy, r) {
     var theta = 360 / 8 ,
       rotateDeg = theta / 2 ,
@@ -66,6 +96,7 @@ var ATField = Class.create({
       ];
     });
   },
+
   insertOctagon: function(elm) {
     this.svgElm.insertBefore(elm, this.svgElm.lastElementChild);
   }
