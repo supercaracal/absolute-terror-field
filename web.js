1d7ab632f25dfb0d3cbea50fdbf8ef34e5147803
supercaracal <supercaracal@yahoo.co.jp>
2016-11-08 12:06:01 +0900

    On Azure!!

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
-var express = require('express')
-  , http = require('http');
-var app = express();
-var server = http.createServer(app);
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app);
+
 server.listen(process.env.PORT || 8080);
+
 app.get('/', function(req, res) {
   res.header('Cache-Control', 'public, max-age=1800');
   res.sendFile(__dirname + '/index.html');
 });
+
 app.get('/*', function(req, res) {
   res.header('Cache-Control', 'public, max-age=1800');
   res.sendFile(__dirname + req.url);
